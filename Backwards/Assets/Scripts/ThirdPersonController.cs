7bbda0fba2e5e0eb0ce4eb9736c73be0e02b06f2
Kyler Nickel <kyler@nickelmusic.com>
2017-09-13 14:03:32 -0700

    Work on player movement, basic temporary model

---
+++ Backwards/Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour {
+
+    public GameObject character;
+    public float speed = 10f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MoveCharacter();
+        
+    }
+
+    void MoveCharacter()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 force = new Vector3(0, 0, speed);
+            Vector3 position = new Vector3(character.transform.position.x, character.transform.position.y - 0.5f, character.transform.position.z);
+            rb.AddForceAtPosition(force, position);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 force = new Vector3(-speed, 0, 0);
+            Vector3 position = new Vector3(character.transform.position.x, character.transform.position.y - 0.5f, character.transform.position.z);
+            rb.AddForceAtPosition(force, position);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 force = new Vector3(0, 0, -speed);
+            Vector3 position = new Vector3(character.transform.position.x, character.transform.position.y - 0.5f, character.transform.position.z);
+            rb.AddForceAtPosition(force, position);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 force = new Vector3(speed, 0, 0);
+            Vector3 position = new Vector3(character.transform.position.x, character.transform.position.y - 0.5f, character.transform.position.z);
+            rb.AddForceAtPosition(force, position);
+        }
+    }
+}
