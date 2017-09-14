93701676e07e7c3386c0ee1e39bbb72586e42864
Cameron Smith <camerosmit@yahoo.com>
2017-09-13 23:17:22 -0700

    Changed Level1 and added the tag "Model" to the player model

---
+++ Backwards/Assets/Shredder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        foreach (Transform child in trigger.transform)
+        {
+            if (child.gameObject.tag == "Model")
+            {
+                Destroy(child.gameObject);
+                Debug.Log("You fell out of the world!");
+                // TODO Add respawning function.
+            }
+        }
+
+    }
+}
